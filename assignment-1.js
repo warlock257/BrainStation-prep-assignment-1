71060665732b88c295f464a2f3e566dce497c34a
David Courtemanche <davidcourtemanche@hotmail.com>
2018-10-09 20:30:41 -0400

    Assignment 1 submission

---
+++ assignment-1.js
@@ -0,0 +1,15 @@
+//Prep course Assignment 1 - David Courtemanche
+
+var numbers = [];
+
+for (var i = 0; i < 10; i++){
+    numbers.push(i);
+}
+
+console.log(numbers[0]);
+console.log(numbers[numbers.length - 1]);
+
+var car = {};
+
+car.color = "black";
+console.log(car);
